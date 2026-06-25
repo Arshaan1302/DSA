f796093e2923828a77aa9904a33635ada3a5f8fd
ashaikh <arshaan_shaikh@zimbra.sevenpv.com>
2026-06-25 16:54:28 +0530

    Arrays basics

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+      
        int n = nums.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -13,3 +17,25 @@ public:
     }
     
 };
+    
+
+int main() {
+    Solution sol;
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cin >> target;
+
+    vector<int> ans = sol.twoSum(nums, target);
+
+    for (int x : ans)
+        cout << x << " ";
+    cout << '\n';
+}
--- concept/array.cpp
+++ concept/array.cpp
@@ -1,7 +1,70 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-int main() {
-    cout << "Hello DSA!" << endl;
+// Input In Array
+void inputArray(int arr[],int size){
+     cout<<"Input Array : "<<endl;
+    for (int i=0 ; i < size ; i++){
+        cin >> arr[i];
+    }
+}
+
+// Print Array
+void outputArray(int arr[],int size){
+    cout<<"Printing Array : "<<endl;
+    for (int i=0 ; i < size ; i++){
+        cout<< arr[i] <<" " ;
+    }
+    cout<<endl;
+}
+// Linear Search 
+bool LinearSearch(int arr[],int size,int target){
+    cout<<"Searching in  Array : "<< target <<endl;
+    for (int i=0;i<size;i++){
+        if(arr[i]==target){
+            return 1;
+        }
+        else{
+           continue;
+        }
+    }
     return 0;
+}
+// Sort
+void sortArray(int arr[], int size){
+    int temp;
+
+    for(int i = 0; i < size - 1; i++){
+        for(int j = i + 1; j < size; j++){
+            if(arr[i] > arr[j]){   
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted Array : ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Arrays
+    int arr[10] ;
+    inputArray(arr,10);
+    outputArray(arr,10);
+    int n;
+    cout<<"Enter Target :"<<endl;
+    cin>>n;
+    if(LinearSearch(arr,10,n)){
+         cout<<"Target "<<n<<" is present "<<endl;
+    }
+    else{
+         cout<<"Error !! Target "<<n<<" is not present "<<endl;
+    }
+    sortArray(arr,10);
 }
